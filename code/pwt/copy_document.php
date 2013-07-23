f65469d67c0f97ecd8f5adce1c01df625dbc0815
root <p_geshev@yahoo.com>
2013-07-23 17:48:39 +0300

    A system file to copy documents

---
+++ code/pwt/copy_document.php
@@ -0,0 +1,16 @@
+<?php
+$docroot = getenv('DOCUMENT_ROOT');
+require_once ($docroot . '/lib/static.php');
+
+global $user;
+
+if((int)$user->staff) {
+	$lDocumentId = (int) $_REQUEST['document_id'];
+	$lCopy = new cdocument_copy(array('document_id' => $lDocumentId));
+	$lCopy->GetData();
+	echo 'copy finished';
+} else {
+	echo 'you have no permissions';
+}
+
+?>
