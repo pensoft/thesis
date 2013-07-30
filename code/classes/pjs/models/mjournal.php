41a0bf3e77ecb5910a1ebe58d4c13b0b50af3cd2
Jordan Biserkov <jordan@biserkov.com>
2013-07-30 10:32:55 +0300

    Suggested reviewers climbing height decreased to 5

--- code/classes/pjs/models/mjournal.php
+++ code/classes/pjs/models/mjournal.php
@@ -127,9 +127,9 @@ class mJournal extends emBase_Model {
 						, u.first_name, u.last_name, u.uname AS email
 						, t1.pos AS tax_pos, t2.pos AS sub_pos, t3.pos AS geo_pos
 	
-						, ( greatest(coalesce(1 - tax_rating::double precision / 10 , 0), 0.0) * 6 + 
-							greatest(coalesce(1 - sub_rating::double precision / 10 , 0), 0.0) * 3 + 
-							greatest(coalesce(1 - geo_rating::double precision / 10 , 0), 0.0) * 1)
+						, ( greatest(coalesce(1 - tax_rating::double precision / 5 , 0), 0.0) * 6 + 
+							greatest(coalesce(1 - sub_rating::double precision / 5 , 0), 0.0) * 3 + 
+							greatest(coalesce(1 - geo_rating::double precision / 5 , 0), 0.0) * 1)
 					
 						  as rating, 3 AS added,
 						  
