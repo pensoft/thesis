9bacd09ac552bd6115626e69713b655030c1fec0
Teodor Georgiev <preprint@pensoft.net>
2013-08-01 08:44:09 +0300

    read only lenta

--- items/languages/en.str.php
+++ items/languages/en.str.php
@@ -2181,7 +2181,7 @@ $STRARRAY = array(
 	'pjs.tooltips.reinvite_reviewer' => 'Invite this reviewer again',
 	'pjs.tooltips.send_email' => 'Send e-mail',
 	'pjs.cannotInviteMoreReviewers' => 'You can not invite more reviewers',
-	
+	'pjs.versionIsReadonly' => 'This is read-only version!'
 	
 );
 ?>
