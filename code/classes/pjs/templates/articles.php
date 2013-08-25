dbc1d94cccc5263d838832b7dc48677dab207fae
Jordan Biserkov <jordan@biserkov.com>
2013-08-25 11:13:48 +0300

    AOF head

--- code/classes/pjs/templates/articles.php
+++ code/classes/pjs/templates/articles.php
@@ -57,6 +57,17 @@ $gTemplArr = array(
 			{*articles.header}
 			<script>SetArticleId({id});</script>
 			<div class="Main-Content">
+				<div id="article-head">
+				<img alt="" src="/i/bdj-eye.png" id="bdj-eye" />
+				<div id="article-id">Biodiversity Data Journal 1:e957 (02.09.2013)<br />
+					 doi: 10.3975/BDJ.1:e957
+				</div>
+				<a href="">PRINT</a>
+				<a href="">PDF</a>
+				<a href="">XML</a>
+				<div class="P-Clear"></div>
+				<hr />
+				</div>
 				<div id="article-preview">
 					<iframe src="/article_preview.php?id={id}" id="articleIframe" style="height: 1600px;"></iframe>
 					<script>
