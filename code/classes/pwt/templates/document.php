cdcd7b8d4fad92bbc4b6beea43ff9f780cda5fae
Victor <vic.penchev@gmail.com>
2013-08-27 17:43:46 +0300

    PWT -  validation template changes

--- code/classes/pwt/templates/document.php
+++ code/classes/pwt/templates/document.php
@@ -157,13 +157,32 @@ $gTemplArr = array(
 								var articleStructure = new articleMenu(\'articleMenu\', \'P-Right-Arrow\' , \'P-Down-Arrow\');
 							</script>
 						</div><!-- End P-Wrapper-Container-Left -->
+						<div class="P-Wrapper-Container-Right {_getContainerHideClass(2)}">
+							{*document.rightcol}
+							<div class="P-Container-Toggler-Btn-Right" onclick="toggleRightContainer();"></div>
+						</div><!-- End P-Wrapper-Container-Right -->
+						<div class="P-Wrapper-Container-Middle {_getContainerHideClass(1)} {_getContainerHideClass(2)}">
+							{xml_validation}
+						</div><!-- End P-Wrapper-Container-Middle -->
+
+						{*tables.tables_popup}
+	',
+	
+	'document.xml_validation_wrapper_no_comments' => '
+						<div class="P-Wrapper-Container-Left {_getContainerHideClass(1)}">
+							{document_structure}
+							<div class="P-Container-Toggler-Btn-Left" onclick="toggleLeftContainer();"></div>
+							<script type="text/javascript">
+								var articleStructure = new articleMenu(\'articleMenu\', \'P-Right-Arrow\' , \'P-Down-Arrow\');
+							</script>
+						</div><!-- End P-Wrapper-Container-Left -->
 						<div class="P-Wrapper-Container-Middle P-Wrapper-Container-Right-Hide">
 							{xml_validation}
 						</div><!-- End P-Wrapper-Container-Middle -->
 
 						{*tables.tables_popup}
 	',
-		
+	
 	'document.tree_ajax_wrapper' => '
 						<div id="document_tree_holder">
 							<script>LoadDocumentTree(\'document_tree_holder\', {document_id}, {instance_id});</script>
