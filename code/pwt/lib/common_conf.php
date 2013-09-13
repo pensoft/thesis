f5356424d0d29880f2559d07dabdcf3c490c0f75
root <p_geshev@yahoo.com>
2013-09-13 09:51:41 +0300

    Article auto unlock interval changed to 60 minutes

--- code/pwt/lib/common_conf.php
+++ code/pwt/lib/common_conf.php
@@ -509,7 +509,7 @@ define('TAXON_NAME_USAGE_FIGURE', 4);
 define('TAXON_NAME_USAGE_TREATMENT', 1);
 define('TAXON_NAME_USAGE_CHECKLIST_TREATMENT', 2);
 define('TAXON_NAME_USAGE_ID_KEY', 3);
-define('DOCUMENT_AUTO_UNLOCK_INTERVAL', 4 * 60);//In seconds
+define('DOCUMENT_AUTO_UNLOCK_INTERVAL', 60 * 60);//In seconds
 
 define('PJS_ARTICLE_AJAX_SRV', PJS_SITE_URL . '/lib/ajax_srv/article_elements_srv.php');
 define('PJS_ARTICLE_TABLE_DL_SRV', PJS_ARTICLE_AJAX_SRV . '?action=download_table_csv&instance_id=');
