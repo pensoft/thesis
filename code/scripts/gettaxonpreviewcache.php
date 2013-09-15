aaeb8c30843824e99bd38afcec57fb043a61a4c6
root <p_geshev@yahoo.com>
2013-09-15 16:03:22 +0300

    Changes to taxon cache generation script

--- code/scripts/gettaxonpreviewcache.php
+++ code/scripts/gettaxonpreviewcache.php
@@ -7,8 +7,14 @@ ini_set('display_errors', 'On');
 ini_set('log_errors', 'On');
 
 $lArticleId = (int)$argv[1];
+$lMinId = (int)$argv[2];
+$lMaxId = (int)$argv[3];
+// var_dump($lArticleId, $lMinId, $lMaxId);
+// exit;
 $lPreview = new ctaxon_cache_generator(array(
 	'article_id' => $lArticleId,
+	'min_id' => $lMinId,
+	'max_id' => $lMaxId,
 ));
 $lPreview->GetData();
 var_dump($lPreview->m_errCnt, $lPreview->m_errMsg);
