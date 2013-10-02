48c9d1e31daf2b8dcd9b79347b955e81b4cb3c0d
Victor <vic.penchev@gmail.com>
2013-10-02 16:06:07 +0300

    PJS - reviewer form changes

--- code/classes/pjs/templates/document_edit.php
+++ code/classes/pjs/templates/document_edit.php
@@ -19,46 +19,7 @@ $gTemplArr = array(
 				</div>
 			</div>
 			<div class="P-Head-Profile-Menu">
-				<div class="userloggedmenu">
-					<a class="userloggedimageA" href="/">
-						{_showProfilePic(previewpicid)}
-					</a>
-					<div class="userloggedimage">
-						<div class="username_left"></div>
-						<div class="username">{fullname}</div>
-						<div class="username_right"></div>
-					</div>
-					<div id="userLoggedMenu" class="userloggedmenulinks">
-						<div class="userloggedmenulinksTop"></div>
-						<div class="userloggedmenulinksMainInner">
-							<div class="userLoggedMenuLink">
-								<a id="userLoggedMenuLink_1" href="#" onclick="LayerProfEditFrm(\'P-Registration-Content\', 2, 1);">Account Settings</a>&nbsp;Login, Password, ...
-							</div>
-							<div class="userloggedmenuSep"></div>
-							<div class="userLoggedMenuLink">
-								<a id="userLoggedMenuLink_2" href="#" onclick="LayerProfEditFrm(\'P-Registration-Content\', 2, 2);">Profile</a>&nbsp;Name, Pictures, URL, ...
-							</div>
-							<div class="userloggedmenuSep"></div>
-							<div class="userLoggedMenuLink">
-								<a id="userLoggedMenuLink_2" href="#" onclick="LayerProfEditFrm(\'P-Registration-Content\', 2, 3);">Subscription</a>&nbsp;Name, Pictures, URL, ...
-							</div>
-							<div class="userloggedmenuSep"></div>
-							<div class="userLoggedMenuLink">
-								<a id="userLoggedMenuLink_2" href="/inbox.php">Messages</a>&nbsp;Inbox, Messages, ...
-								<div class="P-Clear"></div>
-							</div>
-							<div class="userloggedmenuSep"></div>
-							<div class="userLoggedMenuLink">
-								<a id="userLoggedMenuLink_2" href="/login.php?logout=1">Logout</a>
-								<div class="P-Clear"></div>
-							</div>
-							<div class="P-Clear"></div>
-							<div class="userloggedmenuSep h15"></div>
-							<div class="P-Clear"></div>
-						</div>
-						<div class="userloggedmenulinksBottom"></div>
-					</div>
-				</div>
+				{*global.profile_pic_and_name}
 			</div>
 			{_displayReadonlyVersionHeaderBox(version_is_readonly)}
 		</div>
